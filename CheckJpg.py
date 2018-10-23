1f4bfe7080da51bddc64b4435a685e19459585ec
Park-Jongil <always97@nate.com>
2018-10-23 11:24:58 +0900

    2018.10.23 Video Capture Code Append

--- CheckJpg.py
+++ CheckJpg.py
@@ -7,14 +7,13 @@ def showImage():
     # cv2.IMREAD_GRAYSCALE : 흑백 이미지로 로드
     # cv2.IMREAD_UNCHANGED : 알파 채컬을 포함한 이미지 그대로 로드
     image = cv2.imread(FILENAME, cv2.IMREAD_COLOR)
-    cv2.namedWindow('model', cv2.WINDOW_NORMAL)  #윈도우 창의 성격 지정 인자 : (윈도우타이틀, 윈도우 사이즈 플래그) , 사용자가 크기 조절할 수 있는 윈도우 생성
+    cv2.namedWindow('model', cv2.WINDOW_NORMAL  )  #윈도우 창의 성격 지정 인자 : (윈도우타이틀, 윈도우 사이즈 플래그) , 사용자가 크기 조절할 수 있는 윈도우 생성
     cv2.imshow('model', image)  # 화면에 이미지 띄우기 인자;(윈도우타이틀, 이미지객체)
 
-    flag = False;
+    flag = False
     while True :
         k = cv2.waitKey(0) & 0xFF
         if k == 27:         # wait for ESC key to exit
-    #        cv2.destroyAllWindows()
             break
         elif k == ord('s'): # wait for 's' key to save
             flag = not flag
